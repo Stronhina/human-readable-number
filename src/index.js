258693d244e9ce30afb7eef0ebc3d4ae2b8e0d39
Yauheniya Stronhina <stronhinaev@gmail.com>
2021-02-14 21:30:40 +0300

    solve human-readble-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function toReadable (number) {
-  
+    let numberWords;
+    let ones = ["",
+                " one",
+                " two",
+                " three",
+                " four",
+                " five",
+                " six",
+                " seven",
+                " eight",
+                " nine",
+                " ten",
+                " eleven",
+                " twelve",
+                " thirteen",
+                " fourteen",
+                " fifteen",
+                " sixteen",
+                " seventeen",
+                " eighteen",
+                " nineteen" ];
+    let tens = ["",
+                " ten",
+                " twenty",
+                " thirty",
+                " forty",
+                " fifty",
+                " sixty",
+                " seventy",
+                " eighty",
+                " ninety" ];
+    
+    if (number == 0) {
+        return "zero";
+    }
+
+    if (number % 100 < 20 ) {
+        numberWords = ones[number % 100];
+        number = Math.floor(number / 100);
+    } else {
+        numberWords = ones[number % 10];
+        number = Math.floor(number / 10);
+        
+
+        numberWords = tens[number % 10] + numberWords;
+        number = Math.floor(number / 10);
+    }
+
+   if (number == 0) {
+       return numberWords.trim();
+   }
+
+   return (ones[number] + " hundred" + numberWords).trim();
 }
